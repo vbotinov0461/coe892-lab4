2a353555712d4db5a435ef1f604ab84a766c128e
vbotinov0461 <vbotinov@torontomu.ca>
2026-04-03 21:11:50 -0400

    Add files via upload

--- op.py
+++ op.py
@@ -1,67 +1,24 @@
 import requests
 
-URL = "https://coe892-lab4.onrender.com/lab4/"
+# URL = "https://coe892-lab4.onrender.com/lab4/"
+URL = "http://localhost:8000/lab4/"
 
-def get_map():
-    map_data = requests.get(URL + "map")
-    print(f"Got map {map_data.json()}")
-
-
-def update_map(arg1, arg2):
-    response = requests.put(URL + "map", json={"dim_v": arg2, "dim_h": arg1})
-    print(f"Updated {response.json()}")
-
-
-def get_mine(arg):
-    url = URL + "mines" if arg == 0 else URL + f"mines/{arg}"
-    response = requests.get(url)
-    print(f"Response to mine request: {response.json()}")
-
-
-def delete_mine(arg):
-    response = requests.delete(URL + f"mines/{arg}")
-    print(f"Deleted mine {response.json()}")
-
-
-def create_mine(arg):
-    response = requests.post(URL + "mines", json={"x": arg[2], "y": arg[3], "serial": arg[4]})
-    print(f"Created mine {response.json()}")
-
-
+def get_map(): return requests.get(URL + "map")
+def update_map(arg1, arg2): return requests.put(URL + "map", json={"dim_v": arg2, "dim_h": arg1})
+def get_mine(arg): return requests.get(URL + "mines" if arg == 0 else URL + f"mines/{arg}")
+def delete_mine(arg): return requests.delete(URL + f"mines/{arg}")
+def create_mine(arg): return requests.post(URL + "mines", json={"x": arg[2], "y": arg[3], "serial": arg[4]})
 def update_mine(arg):
     payload = {}
     if arg[1] is not None: payload["x"] = arg[1]
     if arg[2] is not None: payload["y"] = arg[2]
     if arg[3] is not None: payload["serial"] = arg[3]
-    response = requests.put(URL + f"mines/{arg}", json=payload)
-    print(f"Updated mine: {response.json()}")
-
-
-def get_rover(arg):
-    response = requests.get(URL + "rovers") if arg == 0 else requests.get(URL + f"rovers/{arg}") 
-    rovers = response.json()
-    if not rovers: print(f"No rovers exist yet.")
-    else: print(f"Response to rover request: {rovers}")
-
-
-def create_rover(arg):
-    response = requests.post(URL + "rovers", json={"instructions": arg})
-    print(f"Created rover: {response.json()}")
-
-
-def delete_rover(arg):
-    response = requests.delete(URL + f"rovers/{arg}")
-    print(f"Deleted rover: {response.json()}")
-
-
-def send_rover(arg):
-    response = requests.put(URL + f"{arg[2]}", json={"instructions": arg[3]})
-    print(f"HANDLE THIS RESPONSE: {response.json()}")
-
-
-def dispatch_rover(arg):
-    response = requests.post(URL + f"rovers/{arg[2]}/dispatch")
-    print(f"Dispatched rover! {response.json()}")
+    return requests.put(URL + f"mines/{arg}", json=payload)
+def get_rover(arg): return requests.get(URL + "rovers") if arg == 0 else requests.get(URL + f"rovers/{arg}") 
+def create_rover(arg): return requests.post(URL + "rovers", json={"instructions": arg})
+def delete_rover(arg): return requests.delete(URL + f"rovers/{arg}")
+def send_rover(arg): return requests.put(URL + f"rovers/{arg[2]}", json={"instructions": arg[3]})
+def dispatch_rover(arg): return requests.post(URL + f"rovers/{arg}/dispatch")
 
 
 if __name__ == '__main__':
@@ -73,19 +30,19 @@ if __name__ == '__main__':
         'quit' to quit the program\n\n""")
         if start == "help":
             print("""\n\nCommand list:
-                map get : Get map file
-                map update {x} {y} : Update x and y dimensions of map
-                mine get : Get mine list
+                map get                                                         : Get map file
+                map update {x} {y}                                              : Update x and y dimensions of map
+                mine get                                                        : Get mine list
                 mine get {id}                                                   : Get info of a specific mine
                 mine delete {id}                                                : Delete a mine
                 mine create {x} {y} {serial}                                    : Create a mine with coordinates and serial number
                 mine update {id} {x}(optional) {y}(optional) {serial}(optional) : Update a mine's information
-                rover get : Get all rover info
-                rover get {id} : Get specific rover info
-                rover create {commands} : Create a rover and provide commandlist
-                rover delete {id} : Delete a rover
-                rover send {commands} : Send a rover some commands
-                rover dispatch {id} : Dispatch a rover to execute its commands)""")
+                rover get                                                       : Get all rover info
+                rover get {id}                                                  : Get specific rover info
+                rover create {commands}                                         : Create a rover and provide commandlist
+                rover delete {id}                                               : Delete a rover
+                rover send {id} {commands}                                      : Send a rover some commands
+                rover dispatch {id}                                             : Dispatch a rover to execute its commands)""")
 
         elif (start == "go") or (start == "quit"): break
         else: print("Unrecognized command")
@@ -93,28 +50,32 @@ if __name__ == '__main__':
     if start == "go":
         while True:
             user_in = input("Please enter a command: ")
-            args = user_in.split()
+            args, result = user_in.split(), {}
 
-            if "map" in args:
-                if "get" in args: get_map()
-                elif ("update" in args) & (len(args) == 4): update_map(args[2], args[3])
+            if args[0]=="map":
+                if args[1]=="get": result = get_map() 
+                elif (args[1]=="update") and (len(args) == 4): result = update_map(args[2], args[3])
                 else: print("Invalid arguments.")
 
-            elif "mine" in args:
-                if ("get" in args) & (len(args) == 2): get_mine(0)
-                elif "get" in args: get_mine(args[2])
-                elif "delete" in args: delete_mine(args[2])
-                elif ("create" in args) & (len(args) == 5): create_mine(args)
-                elif ("update" in args) & (len(args) == 5): update_mine(args)
+            elif args[0]=="mine":
+                if (args[1]=="get") and (len(args) == 2): result = get_mine(0)
+                elif args[1]=="get": result = get_mine(args[2])
+                elif (args[1]=="delete") and (len(args) == 3): result = delete_mine(args[2])
+                elif (args[1]=="create") and (len(args) == 5): result = create_mine(args)
+                elif (args[1]=="update") and (len(args) == 5): result = update_mine(args)
                 else: print("Invalid arguments")
 
-            elif "rover" in args:
-                if ("get" in args) & (len(args) == 2): get_rover(0)
-                elif "get" in args: get_rover(args[2])
-                elif ("create" in args) & (len(args) > 2): create_rover(args[2])
-                elif ("delete" in args) & (len(args) > 2): delete_rover(args[2])
-                elif ("send" in args) & (len(args) > 2): send_rover(args)
-                elif ("dispatch" in args) & (len(args) > 2): dispatch_rover(args[2])
+            elif args[0]=="rover":
+                if (args[1]=="get") and (len(args) == 2): result = get_rover(0)
+                elif args[1]=="get": result = get_rover(args[2])
+                elif (args[1]=="create") and (len(args) == 3): result = create_rover(args[2])
+                elif (args[1]=="delete") and (len(args) == 3): result = delete_rover(args[2])
+                elif (args[1]=="send") and (len(args) == 4): result = send_rover(args)
+                elif (args[1]=="dispatch") and (len(args) == 3): result = dispatch_rover(args[2])
                 else: print("Invalid arguments. Try again, please.")
 
-            else: print("Could not recognize arguments. Try again.")
+            elif args[0]=="quit": break
+
+            print("Invalid command. Try again") if not result else print(f"[Server Response] {result.json()}")
+
+
--- server.py
+++ server.py
@@ -17,28 +17,21 @@ def get_json(fname):
         with open(fname, "r") as f: return json.load(f)
     except json.JSONDecodeError:
         return {}
-
-# Rover Execution Functions
-def turn(ltr, d): return (d + (1 if ltr == "R" else -1)) % 4
-
-
+# facing legend --> SOUTH=0, WEST=1, NORTH=2, EAST=3
+def turn(ltr, d): 
+    print("Turning!")
+    return (d + (1 if ltr == "R" else -1)) % 4
 def move(l, d):
     # Retrieve map boundaries
     data = get_json(map_file)
     x_bound, y_bound = data["cols"], data["rows"]
 
     # Execute movement
-    if (d == 0) and (l[1] < (y_bound-1)):
-        l[1] += 1
-    elif (d == 1) and (l[0] > 0):
-        l[0] -= 1
-    elif (d == 2) and (l[1] > 0):
-        l[1] -= 1
-    elif (d == 3) and (l[0] < (x_bound-1)):
-        l[0] += 1
+    if (d == 0) and (l[1] < (y_bound-1)): l[1] += 1
+    elif (d == 1) and (l[0] > 0): l[0] -= 1
+    elif (d == 2) and (l[1] > 0): l[1] -= 1
+    elif (d == 3) and (l[0] < (x_bound-1)): l[0] += 1
     return l
-
-
 def dig(l):
     sn = ""
     # Check if there's a mine here
@@ -50,6 +43,7 @@ def dig(l):
         for m in mine_data["mines"]:
             if (m[2][0] == l[0]) and (m[2][1] == l[1]):
                 sn = m[1]
+                mine_data["mines"].remove(m)
                 break
         # Brute force using randomly generated PINs
         while 1:
@@ -59,22 +53,23 @@ def dig(l):
                 print(key)
                 map_data = get_json(map_file)
                 map_data["grid"][l[1]][l[0]] = 0
+                with open(map_file, "w") as f: json.dump(map_data, f, indent=4)
+                with open(mines_file, "w") as f: json.dump(mine_data, f, indent=4)
                 break
         print("Mine disarmed!")
 
 
 # -- DEFINE JSON ASSETS --
-rover_dir, map_file, mines_file = "./rovers/", "map.json", "mines.json"
+map_file, mines_file = "map.json", "mines.json"
+rovers, next_rover_id, next_mine_id = [], 1, 3
 
 if not os.path.exists(map_file):
     with open(map_file, "w") as F:
-        json.dump({"grid": [[0, 1, 0], [0, 0, 0], [1, 0, 0], [0, 0, 0]],
-                   "rows": 4, "cols": 3, "next_id": 1}, F)
+        json.dump({"grid": [[0, 1, 0], [0, 0, 0], [1, 0, 0], [0, 0, 0]], "rows": 4, "cols": 3}, F)
 
 if not os.path.exists(mines_file):
     with open(mines_file, "w") as F:
-        json.dump({"mines": [[1, "x1y0", [1, 0]], [2, "x0y2", [0, 2]]],
-                   "next_id": 3}, F)
+        json.dump({"mines": [[1, "x1y0", [1, 0]], [2, "x0y2", [0, 2]]]}, F)
 
 # --- FastAPI Logic ---
 app = FastAPI()
@@ -83,22 +78,16 @@ app = FastAPI()
 class Loc(BaseModel):
     dim_v: int
     dim_h: int
-
-
 class MineInfo(BaseModel):
     x: int
     y:  int
     serial: str
-
-
 class RoverInfo(BaseModel):
     instructions: str
-
 
 # Map & Mines Endpoints
 @app.get("/lab4/map")
 def get_map(): return get_json(map_file)
-
 
 @app.put("/lab4/map")
 def put_map(loc: Loc):
@@ -112,35 +101,29 @@ def put_map(loc: Loc):
     if loc.dim_h > data["cols"]:
         diff = data["cols"] - loc.dim_h
         pad = [0]*diff
-        for r in data["grid"]:
-            r.extend(pad)
+        for r in data["grid"]: r.extend(pad)
         data["cols"] = loc.dim_h
 
     if loc.dim_v < data["rows"]:
         data["grid"] = data["grid"][:loc.dim_v]
         data["rows"] = loc.dim_v
     if loc.dim_h < data["cols"]:
-        for r in data["rows"]:
-            r = r[:loc.dim_h]
+        for r in data["rows"]: r = r[:loc.dim_h]
         data["cols"] = loc.dim_h
 
     with open(map_file, "w") as f: json.dump(data, f, indent=4)
-
+    return {"Success": "Updated"}
 
 @app.get("/lab4/mines")
 def get_mines(): return get_json(mines_file)
-
 
 @app.get("/lab4/mines/{mine_id}")
 def get_mines_id(mine_id: int):
     data, flag = get_json(mines_file), False
     for m in data["mines"]:
-        if m[0] == mine_id:
-            result, flag = m, True
-    if not flag:
-        raise HTTPException(status_code=404, details="Mine not found!")
+        if m[0] == mine_id: result, flag = m, True
+    if not flag: result = {"Failed": "Mine doesn't exist"}
     return result
-
 
 @app.delete("/lab4/mines/{mine_id}")
 def mine_delete(mine_id: int):
@@ -148,143 +131,132 @@ def mine_delete(mine_id: int):
     
     # Check existence
     coords = [-1]
-    for m in data["mines"]:
+    for m in data["mines"]: 
         if m[0] == mine_id: coords = m[2]
-    if coords[0] == -1: raise HTTPException(status_code=404, detail="Mine not found!")
+    if coords[0] == -1: return {"Failed": "Mine doesn't exist"}
     
     # Erase from map file
     map_data = get_json(map_file)
-    map_data["grid"][coords[0]][coords[1]] = 0
+    map_data["grid"][coords[1]][coords[0]] = 0
     with open(map_file, "w") as f: json.dump(map_data, f, indent=4)
     
     # Erase from ref json
     original_len = len(data["mines"])
     data["mines"] = [m for m in data["mines"] if m[0] != mine_id]
     with open(mines_file, "w") as f: json.dump(data, f, indent=4)
-    return {"message": "Deleted"}
-
+    return {"Success": "Deleted"}
 
 @app.post("/lab4/mines")
 def mine_create(body: MineInfo):
+    global next_mine_id
     data = get_json(mines_file)
-    # Update map
-    map_data = get_json(map_file)
-    map_data["grid"][body.x][body.y] = 1
-    with open(map_file, "w") as f: json.dump(map_data, f, indent=4)
-    # Add json record
-    data["mines"].append([data["next_id"], body.serial, [body.x, body.y]])
-    data["next_id"]+=1
-    with open(mines_file, "w") as f: json.dump(data, f, indent=4)
-    return data["next_id"]-1
 
+    # Check if spot is taken
+    map_data = get_json(map_file)
+    if map_data["grid"][body.y][body.x]==1: return {"Failed": "There's already a mine there"}
+    
+    # Update map
+    map_data["grid"][body.y][body.x] = 1
+    with open(map_file, "w") as f: json.dump(map_data, f, indent=4)
+    
+    # Add json record
+    data["mines"].append([next_mine_id, body.serial, [body.x, body.y]])
+    next_mine_id+=1
+    with open(mines_file, "w") as f: json.dump(data, f, indent=4)
+    return {"Success": f"New mine ID is: {next_mine_id-1}"}
 
 @app.put("/lab4/mines/{mine_id}")
 def mine_update(mine_id: int, body: MineInfo):
     data, flag = get_json(mines_file), False
     # Check existence
     for m in data["mines"]:
-        if m[0] == mine_id:
-            flag = True
-            break
-    if not flag: raise HTTPException(status_code=404, details="Mine not found!")
+        if m[0] == mine_id: flag = True
+    if not flag: return {"Failed": "Mine doesn't exist"}
     
     result = data
     for m in data["mines"]:
         if m[0] == mine_id:
-            if body.serial is not None:
-                m[1] = body.serial
+            if body.serial is not None: m[1] = body.serial
+            map_data = get_json(map_file)
             if (body.x is not None) and (body.y is not None):
-                map_data = get_json(map_file)
-                map_data["grid"][m[2][0]][m[2][1]] = 0
-                map_data["grid"][body.x][body.y] = 1
-                with (map_file, "w") as f:
-                    json.dump(map_data, f, indent=4)
+                map_data["grid"][m[2][1]][m[2][0]] = 0
+                map_data["grid"][body.y][body.x] = 1
                 m[2][0], m[2][1] = body.x, body.y
             elif body.x is not None:
-                map_data = get_json(map_file)
-                map_data["grid"][m[2][0]][m[2][1]] = 0
-                map_data["grid"][body.x][m[2][1]] = 1
-                with (map_file, "w") as f:
-                    json.dump(map_data, f, indent=4)
+                map_data["grid"][m[2][1]][m[2][0]] = 0
+                map_data["grid"][body.y][m[2][0]] = 1
                 m[2][0] = body.x
             elif body.y is not None:
-                map_data = get_json(map_file)
-                map_data["grid"][m[2][0]][m[2][1]] = 0
-                map_data["grid"][m[2][0]][body.y] = 1
-                with (map_file, "w") as f:
-                    json.dump(map_data, f, indent=4)
+                map_data["grid"][m[2][1]][m[2][0]] = 0
+                map_data["grid"][m[2][1]][body.x] = 1
                 m[2][1] = body.y
+            with (map_file, "w") as f: json.dump(map_data, f, indent=4)
             result = m
             break
     with open(mines_file, "w") as f: json.dump(data, f, indent=4)
     return result
 
-
 @app.get("/lab4/rovers")
 def get_rovers():
-    return [] if not os.listdir(rover_dir) else os.listdir(rover_dir)
-
+    global rovers
+    return {"Rover list is": "EMPTY"} if len(rovers)==0 else rovers
 
 @app.get("/lab4/rovers/{rover_id}")
 def get_rover_id(rover_id: int):
-    result = "Rover doesn't exist"
-    filepath = os.path.join(rover_dir, f"rover_{rover_id}.json")
-    if os.path.exists(filepath):
-        result = get_json(filepath)
+    global rovers
+    result = {"Failed": "Rover doesn't exist"}
+    if not(len(rovers)==0):
+        for r in rovers:
+            if r["id"] == rover_id: result = r
     return result
-
 
 @app.post("/lab4/rovers")
 def rover_create(body: RoverInfo):
-    data = get_json(map_file)
-    filepath = os.path.join(rover_dir, f"rover_{data['next_id']}.json")
-    with open(filepath, "w") as f:
-        json.dump({"rover": [data["next_id"], "Not Started", [0, 0], body.instructions]}, f)
-    data["next_id"]+=1
-    with open(map_file, "w") as f: json.dump(data, f, indent=4)
-    return "Created Rover"
-
+    global rovers, next_rover_id
+    rovers.append({"id": next_rover_id, "status": "Not Started", "coords": [0, 0], "facing": 0, "instructions": body.instructions})
+    next_rover_id+=1
+    return {"Success": f"Created Rover {next_rover_id-1}"}
 
 @app.delete("/lab4/rovers/{rover_id}")
 def rover_delete(rover_id: int):
-    result = f"Successfully deleted Rover {rover_id}"
-    filepath = os.path.join(rover_dir, f"rover_{rover_id}.json")
-    if os.path.exists(filepath):
-        os.remove(filepath)
-    else:
-        result = f"Could not delete Rover {rover_id}"
-    return result
-
+    global rovers
+    original_len = len(rovers)
+    rovers = [r for r in rovers if r["id"] != rover_id]
+    return {"Failed": "Rover doesn't exist"} if original_len==len(rovers) else {"Success": f"Successfully deleted Rover {rover_id}"}
 
 @app.put("/lab4/rovers/{rover_id}")
 def rover_sendInst(rover_id: int, body: RoverInfo):
-    result = f"Successfully passed instructions to Rover {rover_id}"
-    filepath = os.path.join(rover_dir, f"rover_{rover_id}.json")
-    if os.path.exists(filepath):
-        data = get_json(filepath)
-        data["rover"][3] = body.instructions
-        with open(filepath, "w") as f: json.dump(data, f, indent=4)
-    else:
-        result = f"Could not pass instructions to Rover {rover_id}"
-    return result
-
+    global rovers
+    found = False
+    for r in rovers:
+        if r["id"]==rover_id:
+            r["instructions"] = body.instructions
+            found = True
+    return {"Success": f"Successfully passed instructions to Rover {rover_id}"} if found else {"Failed": f"Could not pass instructions to Rover {rover_id}"}
 
 @app.post("/lab4/rovers/{rover_id}/dispatch")
 def rover_dispatch(rover_id: int):
-    # local rover variables
-    filepath = os.path.join(rover_dir, f"rover_{rover_id}.json")
-    facing, data = 0, get_json(filepath)
-    for c in data["rover"][3]:
-        if (c == "L") or (c == "R"): facing = turn(c, facing)
+    global rovers
+    target = {}
+    for r in rovers:
+        if r["id"]==rover_id: target = r
+    if not target: return {"Failed": "Rover doesn't exist"}
+
+    target["status"] = "Moving"
+    for c in target["instructions"]:
+        if (c == "L") or (c == "R"): target["facing"] = turn(c, target["facing"])
         elif c == "M":
             # Check if we are leaving a mine
             map_data = get_json(map_file)
-            if map_data["grid"][data["rover"][2][1]][data["rover"][2][0]] == 1: break
-            else: data["rover"][2] = move(data["rover"][2], facing)
-        elif c == "D": dig(data["rover"][2])
+            if map_data["grid"][target["coords"][1]][target["coords"][0]] == 1: 
+                print(f"[R{target["id"]}] Blew UP!")
+                map_data["grid"][target["coords"][1]][target["coords"][0]] = 0
+                with open(map_file, "w") as f: json.dump(map_data, f, indent=4) 
+                target["status"] = "Eliminated"
+                break
+            else: target["coords"] = move(target["coords"], target["facing"])
+        elif c == "D": dig(target["coords"])
     
     # Mark as Finished
-    data = get_json(filepath)
-    if data["rover"][1] != "Eliminated": data["rover"][1] = "Finished"
-    with open(filepath, "w") as f: json.dump(data, f, indent=4)
-    return data
+    if target["status"] != "Eliminated": target["status"] = "Finished"
+    return target
